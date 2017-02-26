504179bb61e935b6141c82fd4bc3fc4a92acbcc6
ab4854 <ab4854@rit.edu>
2017-02-26 01:00:39 -0500

    Re-created New Console Window
    Does absoulety nothing, just set up the UI.

--- HelloWorldWH/HelloWorldWH/Form1.Designer.cs
+++ HelloWorldWH/HelloWorldWH/Form1.Designer.cs
@@ -28,11 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.codeInputText = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.hintButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(766, 804);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(153, 70);
+            this.submitButton.TabIndex = 0;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(44, 804);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(153, 70);
+            this.clearButton.TabIndex = 1;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            // 
+            // codeInputText
+            // 
+            this.codeInputText.Location = new System.Drawing.Point(44, 257);
+            this.codeInputText.Name = "codeInputText";
+            this.codeInputText.Size = new System.Drawing.Size(875, 505);
+            this.codeInputText.TabIndex = 2;
+            this.codeInputText.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 193);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Please Input Code:";
+            // 
+            // hintButton
+            // 
+            this.hintButton.Location = new System.Drawing.Point(766, 83);
+            this.hintButton.Name = "hintButton";
+            this.hintButton.Size = new System.Drawing.Size(153, 70);
+            this.hintButton.TabIndex = 4;
+            this.hintButton.Text = "Hint";
+            this.hintButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(974, 929);
+            this.Controls.Add(this.hintButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.codeInputText);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.submitButton);
+            this.Name = "Form1";
+            this.Text = "Console Window";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.RichTextBox codeInputText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button hintButton;
     }
 }
