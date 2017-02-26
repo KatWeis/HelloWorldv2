7505ac72043faabbd95bf96ca42fc89c370bdbd6
Katarina Weis <kxw4589@g.rit.edu>
2017-02-25 19:13:03 -0500

    Added in basics for collision detection -- untested

--- HelloWorldWH/HelloWorldWH/Entity.cs
+++ HelloWorldWH/HelloWorldWH/Entity.cs
@@ -12,14 +12,39 @@ namespace HelloWorldWH
     class Entity
     {
         // Attributes
-        Rectangle rec;
-        Texture2D texture;
+        protected Rectangle rec;
+        protected Texture2D texture;
+
+        //properties
+        public Rectangle Rec
+        {
+            get { return rec; }
+        }
+        public Texture2D Texture
+        {
+            get { return texture; }
+        }
 
         // constructor
         public Entity(Rectangle rectangle, Texture2D tex)
         {
             rec = rectangle;
             texture = tex;
+        }
+
+        public bool CollisionDetection(Entity obj)
+        {
+            if(rec.Intersects(obj.Rec))
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+        protected virtual void CollisionResolution(Entity obj, KeyboardState kbState)
+        {
+            
         }
 
     }
--- HelloWorldWH/HelloWorldWH/Player.cs
+++ HelloWorldWH/HelloWorldWH/Player.cs
@@ -11,9 +11,100 @@ namespace HelloWorldWH
 {
     class Player: Entity
     {
+        //variables
+        private const float GRAVITY = 9.8f;
+        //booleans to do with jumping
+        private bool isJumping;
+        private bool canJump;
+        //variable for Y before jumping
+        private int startingY; 
+
         //constructor
         public Player(Rectangle rec, Texture2D tex):base(rec, tex)
         {
+            //initialize jumping bools
+            isJumping = false;
+            canJump = false;
+        }
+
+        /// <summary>
+        /// Resolves collision for platforms
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="kbState"></param>
+        protected override void CollisionResolution(Entity obj, KeyboardState kbState)
+        {
+            //determines if the player is running into a wall
+            if (rec.Bottom > obj.Rec.Top + GRAVITY && isJumping == false)
+            {
+                if (rec.Right > obj.Rec.Left && kbState.IsKeyDown(Keys.D))
+                {
+                    rec.X = obj.Rec.Left - rec.Width;
+                    //position.X -= movement.PlayerSpeed;
+                }
+                else if (rec.Left < obj.Rec.Right && kbState.IsKeyDown(Keys.A))
+                {
+                    rec.X = obj.Rec.Right;
+                    //position.X += movement.PlayerSpeed;
+                }
+            }
+            //sets player on top of terrain if fell
+            if (rec.Bottom <= obj.Rec.Top + GRAVITY && rec.Bottom > obj.Rec.Top)
+            {
+                rec.Y -= rec.Bottom - obj.Rec.Top;
+                startingY = rec.Y;
+                canJump = true;
+            }
+            if (startingY > obj.Rec.Bottom && isJumping == true) // starts below the object & jumps
+            {
+                if (rec.Top < obj.Rec.Bottom && isJumping == true && rec.Top > obj.Rec.Top)
+                {
+                    rec.Y += obj.Rec.Bottom - rec.Top;
+                }
+            }
+            else if (startingY - rec.Height < obj.Rec.Bottom && isJumping == true) // jumps and hits an object from the side
+            {
+                if (rec.Right > obj.Rec.Left && kbState.IsKeyDown(Keys.D))
+                {
+                    rec.X = obj.Rec.Left - rec.Width;
+                    //position.X -= movement.PlayerSpeed;
+                }
+                else if (rec.Left < obj.Rec.Right && kbState.IsKeyDown(Keys.A))
+                {
+                    rec.X = obj.Rec.Right;
+                    //position.X += movement.PlayerSpeed;
+                }
+            }
+        }
+
+        public void PlayerUpdate(List<Entity> stuff, KeyboardState kbState)
+        {
+            foreach(Entity e in stuff)
+            {
+                //standing on top of the platform
+                if (!(rec.Left > e.Rec.Right) && !(rec.Right < e.Rec.Left))
+                {
+                    if (rec.Bottom == e.Rec.Top)
+                    {
+                        isJumping = false;
+                        startingY = rec.Y;
+                        canJump = true;
+                    }
+                }
+                //colliding with platforms
+                if (CollisionDetection(e) == true)
+                {
+                    if(e is Collectable)
+                    {
+                        //call some method to react to colliding with the collectable
+                    }
+                    else
+                    {
+                        CollisionResolution(e, kbState);
+                    }
+                }
+            }
+            
         }
     }
 }
