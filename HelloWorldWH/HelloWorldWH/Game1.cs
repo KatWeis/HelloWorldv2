1695940aa13e641008c6737f4cbf9d3eee6c4d14
Katarina Weis <kxw4589@g.rit.edu>
2017-02-26 10:35:31 -0500

    Added separate locking of right and left movement and collecting stuff/score

--- HelloWorldWH/HelloWorldWH/Game1.cs
+++ HelloWorldWH/HelloWorldWH/Game1.cs
@@ -200,8 +200,24 @@ namespace HelloWorldWH
                             //activated when returning to game version
                             form.Visible = false;
                             gameState = GameState.Game;
-                            player.ScoreOn = true;
-                            player.CanMove = true;
+
+                            switch (formCount)
+                            {
+                                case 0:
+                                    player.CanMove = true; player.CanMoveR = true;
+                                    break;
+                                case 1:
+                                    player.CanMoveL = true;
+                                    break;
+                                case 2:
+                                    form.cs = CodeState.Jump;
+                                    break;
+                                case 3:
+                                    player.ScoreOn = true;
+                                    break;
+                            }
+                            
+                            
 
                             //increment form counter so next time it is the next coding challenge
                             formCount++;
@@ -262,6 +278,7 @@ namespace HelloWorldWH
                 case GameState.Coding:
                     {
                         spriteBatch.Draw(menu, new Rectangle(-40, -15, 880, 520), Color.Black);
+                        spriteBatch.DrawString(font, "Press esc to exit the console", new Vector2(280, 430), Color.PaleTurquoise);
                     }
                     break;
             }
